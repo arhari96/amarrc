7e819ced2fd83692afd7b27171b5e9eb2edfc63d
Hari <haribaburamakrishnan@gmail.com>
2025-01-20 14:01:10 +0530

    cors added

--- amarrc/settings.py
+++ amarrc/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -72,7 +73,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 'django.middleware.csrf.CsrfViewMiddleware',
-'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "amarrc.urls"
@@ -149,6 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 FONTS_ROOT = os.path.join(BASE_DIR, "fonts")
+CORS_ALLOW_ALL_ORIGINS = True
 
 FLUTTER_WEB_APP = os.path.join(BASE_DIR, "flutter_web_app")
 REACT_APP_BUILD_DIR = os.path.join(BASE_DIR, "webfrontend", "build")
