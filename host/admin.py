62309321f4489906b1b74cf3c225dbc316d3b35b
Hari <haribaburamakrishnan@gmail.com>
2026-04-15 20:37:04 +0530

    host added

--- host/admin.py
+++ host/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import HostDetail,CanCall
 
 # Register your models here.
+admin.site.register([HostDetail,CanCall])
--- host/urls.py
+++ host/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HostDetailCreateUpdateView, HostListView, SendMailView
+from .views import HostDetailCreateUpdateView, HostListView, SendMailView, CanCallView
 
 urlpatterns = [
     path('host/', HostDetailCreateUpdateView.as_view(), name='host-create-update'),
     path('hosts/', HostListView.as_view(), name='host-list'),
         path('send-mail/', SendMailView.as_view(), name='send-mail'),
+    path('can-call/', CanCallView.as_view(), name='can-call'),
 
 ]
